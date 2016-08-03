3bbe7e954f5a7a36c07f9eae65d7568381116ff8
butcallmejo <josquin.gaillard@gmail.com>
2016-08-02 19:01:39 -0700

    progress on ec2 project, working with optparse

--- infrastructure_as_code_with_aws_ec2/manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/manage_ec2_instances.rb
@@ -2,9 +2,25 @@ require 'aws-sdk'
 require 'optparse'
 require 'yaml'
 
-class EC2Instance
-	def initialize instance_id
+# get the configuration from the yaml file
+config = YAML.load_file('config.yaml')
 
+# Segment to parse the command line for options
+options = {}
+opts_parser = OptionParser.new do |opts|
+	opts.separator " "
+    opts.separator "Specific options:"
+	opts.banner = "Usage: manage_ec2_instances.rb [options]"
+	opts.on("-a", "--action ACTION", [:launch, :stop, :start, :terminate], "comment") do |action|
+		if action == launch
+			# TODO
+		end
 	end
+	opts.on() do ||
+		# TODO
+	end
+end
+
+def initialize instance_id
 
 end
