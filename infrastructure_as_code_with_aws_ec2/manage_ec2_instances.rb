363a5b41725891163e3036b66723e008855f6843
butcallmejo <josquin.gaillard@gmail.com>
2016-08-02 21:51:15 -0700

    finished parser and trying to connect to ec2, but with failure. debug mode on

--- infrastructure_as_code_with_aws_ec2/manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/manage_ec2_instances.rb
@@ -1,9 +1,8 @@
+#!/usr/bin/ruby
+
 require 'aws-sdk'
 require 'optparse'
 require 'yaml'
-
-# get the configuration from the yaml file
-config = YAML.load_file('config.yaml')
 
 # Segment to parse the command line for options
 options = {}
@@ -33,3 +32,33 @@ opts_parser = OptionParser.new do |opts|
 	end
 end
 opts_parser.parse!
+
+# get the configuration from the yaml file
+config = YAML.load_file('config.yaml')
+
+# setting up the ec2 instance from the config file
+instance = Aws::EC2::Client.new(
+		region: config['us-west-2'],
+		access_key_id: config['access_key_id'],
+		secret_access_key: config['secret_access_key']
+	)
+
+# launching ec2 instance
+if options[:action] == :launch then
+	r = instance.run_instances({
+		image_id: config["image_id"],
+		min_count: 1,
+		max_count: 1,
+		key_name: config['key_pair'],
+		security_group_ids: config["security_group_ids"],
+		instance_type: config["instance_type"],
+		placement: {
+			availability_zone: config["availability-zone"]
+		}
+	})
+	instance_id = r.instances[0].instance_id
+	r = ec2.wait_until(:instance_running, instance_ids:[instance_id])
+	puts instance_id, r.reservations[0].instances[0].public_dns_name
+else
+	puts "something else"
+end
