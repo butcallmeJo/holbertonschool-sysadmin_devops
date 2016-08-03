d4d9ed9e0b706322ff081d70e5dd502af17d64b0
butcallmejo <josquin.gaillard@gmail.com>
2016-08-02 20:48:29 -0700

    added more arguments to parse for

--- infrastructure_as_code_with_aws_ec2/manage_ec2_instances.rb
+++ infrastructure_as_code_with_aws_ec2/manage_ec2_instances.rb
@@ -8,19 +8,23 @@ config = YAML.load_file('config.yaml')
 # Segment to parse the command line for options
 options = {}
 opts_parser = OptionParser.new do |opts|
-	opts.separator " "
+	opts.separator ""
     opts.separator "Specific options:"
 	opts.banner = "Usage: manage_ec2_instances.rb [options]"
-	opts.on("-a", "--action ACTION", [:launch, :stop, :start, :terminate], "comment") do |action|
-		if action == launch
-			# TODO
+	#
+	opts.on("-a", "--action ACTION", [:launch, :stop, :start, :terminate], "Takes an action: launch, stop, start, terminate with a server_id") do |action|
+		if not action then
+			puts "Error: Not enought arguments"
+		else
+			options[:actions] = action
 		end
 	end
-	opts.on() do ||
-		# TODO
+	#
+	opts.on("-i", "--server_id SERVER_ID", "provides the server id when required") do |id|
+		options[:server_id] = id
 	end
-end
-
-def initialize instance_id
-
+	#
+	opts.on("-v", "--verbose", "Provides extra information while the script is running") do |verb|
+		options[:verbose] = verb
+	end
 end
